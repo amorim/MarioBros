6030ce260d09818de22700fb2e245142bdcfbe42
danihcv <daniel.vassalo@hotmail.com.br>
2016-10-25 12:27:21 -0300

    Update startup.h

--- startup.h
+++ startup.h
@@ -1,7 +1,3 @@
-//
-// Created by daniel on 11/10/16.
-//
-
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
@@ -9,14 +5,7 @@
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
 #include <string.h>
-#define URI_BLOCK "/home/daniel/resources/Sprites/brickMarrom.png"
-#define URI_MARIO "/home/daniel/resources/Sprites/Mario/Mario.png"
-#define URI_MarioImgVoltandoPequeno "/home/daniel/resources/Sprites/Mario/Mario-Tempo-Pequeno.png"
-#define URI_MarioImgVoltandoGrande "/home/daniel/resources/Sprites/Mario/Mario-Tempo-Grande.png"
-#define URI_MARIO_DEAD "/home/daniel/resources/Sprites/Mario/Mario-Dead.png"
-#define URI_BIG_MARIO "/home/daniel/resources/Sprites/Mario/bigmario.png"
-#define URI_BG "/home/daniel/resources/Sprites/Background/1/bg1-1.png"
-#define CHAO 60
+
 int chaoGlobal = 60;
 const int LARGURA_TELA = 840;
 const int ALTURA_TELA = 680;
