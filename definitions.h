055eee84118afc87c51cf35ad63a9fa6c2122fb0
danihcv <daniel.vassalo@hotmail.com.br>
2016-10-25 12:22:27 -0300

    Update definitions.h

--- definitions.h
+++ definitions.h
@@ -1,15 +1,54 @@
-//
-// Created by Lucas Amorim on 10/16/16.
-//
-
-#ifndef DETECCAOCOLISAO_DEFINITIONS_H
-#define DETECCAOCOLISAO_DEFINITIONS_H
-
-#endif //DETECCAOCOLISAO_DEFINITIONS_H
 int itemBlocksId[500], qntdItemBlocks = 0;
 int coinsId[500], qntdCoins = 0;
 int enemiesId[500], qntdEnemies = 0;
 int qntdAtualBlocos = 0;
+
+
+#define URI_BRICK "/home/daniel/resources/Sprites/Misc/Tileset/brickMarrom.png"
+#define URI_COIN "/home/daniel/resources/Sprites/Misc/coin.png"
+#define URI_GOOMBA "/home/daniel/resources/Sprites/Enemies/Goomba/Goomba.png"
+#define URI_MUSHROOM "/home/daniel/resources/Sprites/mushroom.png"
+#define URI_QUESTION_BLOCK "/home/daniel/resources/Sprites/Misc/Special_Blocks/Block_Interrogation/blockInterrogationUnused.png"
+#define URI_SOLID_BLOCK "/home/daniel/resources/Sprites/solidblock.png"
+
+#define URI_TelaInicial "/home/daniel/resources/Sprites/Misc/Logo.png"
+#define URI_TelaPause "/home/daniel/resources/Sprites/Misc/TelaPause.png"
+#define URI_TelaInicialPressStart "/home/daniel/resources/Sprites/Misc/Press Enter.png"
+#define URI_TelaSelecaoLevel "/home/daniel/resources/Sprites/Misc/Mapa/Mapa.png"
+#define URI_TelaSelecaoLevelStatus1 "/home/daniel/resources/Sprites/Misc/Mapa/Mapa-base-Disponivel.png"
+#define URI_TelaSelecaoLevelStatus2 "/home/daniel/resources/Sprites/Misc/Mapa/Mapa-base-Passado.png"
+#define URI_MarioSelecaolevel "/home/daniel/resources/Sprites/Misc/Mapa/Mapa-Mario.png"
+#define URI_TelaInicialCursor "/home/daniel/resources/Sprites/Misc/Items/cogumelo.png"
+
+#define URI_MapaArvoreCoords "/home/daniel/resources/Sprites/Misc/Mapa/arvore coords.txt"
+#define URI_MapaLevelsLiberados "/home/daniel/resources/Sprites/Misc/Mapa/levelPassados.txt"
+
+#define URI_Ranking "/home/daniel/resources/Sprites/Misc/ranking.txt"
+#define URI_TelaRanking "/home/daniel/resources/Sprites/Misc/TelaRanking.png"
+
+#define URI_ThemeSong "/home/daniel/resources/sounds/themeSong1-1.ogg"
+#define URI_TelaInicialSong "/home/daniel/resources/sounds/menuSong.ogg"
+#define URI_JumpSound "/home/daniel/resources/sounds/jumpSound.ogg"
+#define URI_BreakBlockSound "/home/daniel/resources/sounds/breakBlockSound.ogg"
+#define URI_CoinSound "/home/daniel/resources/sounds/coinSound.ogg"
+#define URI_PauseSound "/home/daniel/resources/sounds/pauseSound.ogg"
+#define URI_StompSound "/home/daniel/resources/sounds/stomp.ogg"
+#define URI_DEATH_SOUND "/home/daniel/resources/sounds/mariodie.ogg"
+#define URI_TroSound "/home/daniel/resources/sounds/tro.ogg"
+#define URI_OneUpSound "/home/daniel/resources/sounds/oneup.ogg"
+#define URI_GameOverSound "/home/daniel/resources/sounds/gameOverSound.ogg"
+#define URI_PowerUp "/home/daniel/resources/sounds/powerup.ogg"
+
+#define tempoMaxVoltaNoTempo 10
+
+#define URI_BLOCK "/home/daniel/resources/Sprites/brickMarrom.png"
+#define URI_MARIO "/home/daniel/resources/Sprites/Mario/Mario.png"
+#define URI_MarioImgVoltandoPequeno "/home/daniel/resources/Sprites/Mario/Mario-Tempo-Pequeno.png"
+#define URI_MarioImgVoltandoGrande "/home/daniel/resources/Sprites/Mario/Mario-Tempo-Grande.png"
+#define URI_MARIO_DEAD "/home/daniel/resources/Sprites/Mario/Mario-Dead.png"
+#define URI_BIG_MARIO "/home/daniel/resources/Sprites/Mario/bigmario.png"
+#define URI_BG "/home/daniel/resources/Sprites/Background/1/bg1-1.png"
+#define CHAO 60
 
 #define BRICK 1
 #define COIN 2
